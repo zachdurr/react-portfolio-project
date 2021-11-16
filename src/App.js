cf5508634e9e26671ea9c5029c8fe239e94937a0
zdurr22 <zdurr22@gmail.com>
2021-11-16 11:37:54 -0500

    add navbar components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 import TweetsContainer from './containers/TweetsContainer';
 
+// import Home from './components/navbar/Home';
+// import About from './components/navbar/About';
+// import Login from './components/navbar/Login';
+// import Navbar from './components/navbar/Navbar';
+// import { BrowserRouter as Router, Route} from 'react-router-dom';
+
 class  App extends Component {
   render() {
     return (
       <div className="App">
+          {/* <Router>
+
+              <Navbar />
+              <Route exact path="/" component={Home} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/login" component={Login} />
+     
+          </Router>, */}
         <TweetsContainer />
       </div>
     );
---
+++ src/components/navbar/About.js
@@ -0,0 +1,9 @@
+import React from "react"
+const About = () => {
+    return (
+      <div>
+        <h1>This is about my component!</h1>
+      </div>
+    )
+  }
+export default About
